2050dfa1130d92c9842b65c6812c8a02240a8ea4
Juiceyy <75553966+JuiceyBeans@users.noreply.github.com>
2023-06-11 22:06:50 +0400

    Added blank smithing template

--- src/main/java/com/juicey/jtweaks/JTweaks.java
+++ src/main/java/com/juicey/jtweaks/JTweaks.java
@@ -1,22 +1,21 @@
 package com.juicey.jtweaks;
 
+import com.juicey.jtweaks.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class JTweaks implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("jtweaks");
+	public static final String MOD_ID = "jtweaks";
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello from JuiceyTweaks! Beginning initialisation (beep boop boop beep)");
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/com/juicey/jtweaks/JTweaksDataGenerator.java
+++ src/main/java/com/juicey/jtweaks/JTweaksDataGenerator.java
@@ -3,7 +3,7 @@ package com.juicey.jtweaks;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+public class JTweaksDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
