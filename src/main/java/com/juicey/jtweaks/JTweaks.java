193e47cf475d17dd2c2b9ceb47a05224038e5285
Juiceyy <75553966+JuiceyBeans@users.noreply.github.com>
2023-07-25 12:27:12 +0400

    Update JTweaks.java
    Remove comments

--- src/main/java/com/juicey/jtweaks/JTweaks.java
+++ src/main/java/com/juicey/jtweaks/JTweaks.java
@@ -8,9 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JTweaks implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
     public static final Logger LOGGER = LoggerFactory.getLogger("jtweaks");
 	public static final String MOD_ID = "jtweaks";
 
