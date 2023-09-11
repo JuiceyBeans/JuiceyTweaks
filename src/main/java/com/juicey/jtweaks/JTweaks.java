f0f9f3b4ce25aafc7500b75ed9d564edd70458b2
Juiceyy <75553966+JuiceyBeans@users.noreply.github.com>
2023-09-11 19:39:18 +0400

    Reduced diamond smelting time (read desc.)
    A diamond smelts 128 items now. Would've done 512, but ``fuelTime`` is a short apparently so it might cause chunk saving errors lol.

--- src/main/java/com/juicey/jtweaks/JTweaks.java
+++ src/main/java/com/juicey/jtweaks/JTweaks.java
@@ -18,6 +18,12 @@ public class JTweaks implements ModInitializer {
 		LOGGER.info("Hello from JuiceyTweaks! Beginning initialisation (beep boop boop beep)");
 		ModItems.registerModItems();
 		ModItemGroup.registerItemGroups();
-		FuelRegistry.INSTANCE.add(Items.DIAMOND, 102400);
+
+		// Smelts 128 items
+		// Would've done 512, but fuelTime is a short apparently so it might cause chunk saving errors
+		// Why so long? I guess to get around the non-stackable limit of the lava bucket lol
+		// You could smelt a whole lotta things like this. Full stack would smelt 128 stacks of items
+		// No idea why the hell you'd need to smelt that much lmao
+		FuelRegistry.INSTANCE.add(Items.DIAMOND, 25600);
 	}
 }
