8c3ffc536f75f4cd956c153636ae90055b9e3986
Juiceyy <75553966+JuiceyBeans@users.noreply.github.com>
2023-09-12 13:39:37 +0400

    Snowballs extinguishes entities that are on fire now

--- src/main/java/com/juicey/jtweaks/mixin/SnowballFreezingMixin.java
+++ src/main/java/com/juicey/jtweaks/mixin/SnowballFreezingMixin.java
@@ -1,14 +1,21 @@
 package com.juicey.jtweaks.mixin;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.thrown.SnowballEntity;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 @Mixin(SnowballEntity.class)
-public class SnowballFreezingMixin {
+public abstract class SnowballFreezingMixin extends Entity {
+    public SnowballFreezingMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
 
     /* Used to make snowballs do 1HP damage to all mobs, not what I want but might be useful for future reference
     @ModifyConstant(
@@ -21,6 +28,14 @@ public class SnowballFreezingMixin {
 
     @Inject(method = "onEntityHit", at = @At(value = "TAIL"))
     private void setFreezing(EntityHitResult entityHitResult, CallbackInfo info) {
-        entityHitResult.getEntity().setFrozenTicks(300);
+        Entity entity = entityHitResult.getEntity();
+        entity.setFrozenTicks(300); //Gives freezing to the entity
+        //Extinguishes the entity if it is on fire
+        if (entity.wasOnFire) {
+            entity.playSound(SoundEvents.ENTITY_GENERIC_EXTINGUISH_FIRE, 0.7F, 1.6F + (this.random.nextFloat() - this.random.nextFloat()) * 0.4F);;
+        }
+        if (entity.isOnFire()) {
+            entity.extinguishWithSound();
+        }
     }
 }
