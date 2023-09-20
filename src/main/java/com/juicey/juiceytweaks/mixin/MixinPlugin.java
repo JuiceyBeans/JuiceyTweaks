a8349d823f7524bbdc1c28f6bcaeb909b375003d
Juiceyy <75553966+JuiceyBeans@users.noreply.github.com>
2023-09-20 21:12:37 +0400

    Updated config management

--- src/main/java/com/juicey/juiceytweaks/mixin/MixinPlugin.java
+++ src/main/java/com/juicey/juiceytweaks/mixin/MixinPlugin.java
@@ -1,5 +1,7 @@
 package com.juicey.juiceytweaks.mixin;
 
+import com.juicey.juiceytweaks.JuiceyTweaks;
+import com.juicey.juiceytweaks.config.ModConfig;
 import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.objectweb.asm.tree.ClassNode;
@@ -10,11 +12,30 @@ import java.util.List;
 import java.util.Set;
 
 public class MixinPlugin implements IMixinConfigPlugin {
+    public static MixinPlugin INSTANCE;
+    public static void init() {
+        INSTANCE = new MixinPlugin();
+    }
 
-    //Mixin config adapted from BedrockIfy (https://github.com/juancarloscp52/BedrockIfy)
+    private boolean enableTorchflowerMixin() {
+        return ModConfig.INSTANCE.enableTorchflower;
+    }
+
+    private boolean enableCookieReworkMixin() {
+        return ModConfig.INSTANCE.enableCookieRework;
+    }
+
+    private boolean enableFreezingSnowballsMixin() {
+        return ModConfig.INSTANCE.enableFreezingSnowballs;
+    }
+
+    private boolean enableSculkParticlesMixin() {
+        return ModConfig.INSTANCE.enableSculkParticles;
+    }
+
     @Override
     public void onLoad(String mixinPackage) {
-        MixinFeatureManager.loadMixinSettings();
+
     }
 
     @Override
@@ -24,11 +45,41 @@ public class MixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+//        Object[] sculkMixins = {
+//                "com.juicey.juiceytweaks.mixin.SculkMixin",
+//                "com.juicey.juiceytweaks.mixin.SculkCatalystMixin",
+//                "com.juicey.juiceytweaks.mixin.SculkSensorMixin",
+//                "com.juicey.juiceytweaks.mixin.SculkShriekerMixin",
+//                "com.juicey.juiceytweaks.mixin.SculkVeinMixin",
+//        };
+//
+//        for (Object sculkMixin : sculkMixins) {
+//            if ((mixinClassName.equals(sculkMixin)) && (ModConfig.INSTANCE != null) && !ModConfig.INSTANCE.enableSculkParticles) {
+//                return false;
+//            }
+//        }
+
         if(mixinClassName.equals("com.juicey.juiceytweaks.mixin.TorchflowerGlowMixin") && FabricLoader.getInstance().isModLoaded("glowing-torchflower")) {
-            LogManager.getLogger().info("Glowing Torchflower was detected, disabling torchflower glow from Juicey's Tweaks");
+            JuiceyTweaks.LOGGER.info("[JuiceyTweaks] Glowing Torchflower was detected, disabled glowing torchflowers");
             return false;
         }
-        return MixinFeatureManager.isFeatureEnabled(mixinClassName);
+
+        if(mixinClassName.equals("com.juicey.juiceytweaks.mixin.TorchflowerGlowMixin") && (ModConfig.INSTANCE != null) && !ModConfig.INSTANCE.enableTorchflower) {
+            JuiceyTweaks.LOGGER.info("[JuiceyTweaks] Disabled glowing torchflowers");
+            return false;
+        }
+
+        if(mixinClassName.equals("com.juicey.juiceytweaks.mixin.SnowballFreezingMixin") && (ModConfig.INSTANCE != null) && !ModConfig.INSTANCE.enableFreezingSnowballs) {
+            JuiceyTweaks.LOGGER.info("[JuiceyTweaks] Disabled freezing snowballs");
+            return false;
+        }
+
+        if(mixinClassName.equals("com.juicey.juiceytweaks.mixin.FoodMixin") && (ModConfig.INSTANCE != null) && !ModConfig.INSTANCE.enableCookieRework) {
+            JuiceyTweaks.LOGGER.info("[JuiceyTweaks] Disabled better cookies");
+            return false;
+        }
+
+        return true;
     }
 
     @Override
