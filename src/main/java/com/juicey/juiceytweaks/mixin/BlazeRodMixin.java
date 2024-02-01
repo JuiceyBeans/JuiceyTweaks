829fd7e7ac545ffa9150d81d390df6baec73fafe
Alyx <75553966+JuiceyBeans@users.noreply.github.com>
2024-02-01 17:10:56 +0000

    Blaze rods and blaze powder are fireproof

---
+++ src/main/java/com/juicey/juiceytweaks/mixin/BlazeRodMixin.java
@@ -0,0 +1,32 @@
+package com.juicey.juiceytweaks.mixin;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(Items.class)
+public class BlazeRodMixin {
+
+    @ModifyArg(
+            method = "<clinit>",
+            slice = @Slice(
+                    from = @At(
+                            value = "CONSTANT",
+                            args = "stringValue=blaze_rod"
+                    )
+            ),
+            at = @At(
+                    value = "INVOKE",
+                    target = "net/minecraft/item/Item.<init> (Lnet/minecraft/item/Item$Settings;)V",
+                    ordinal = 0
+
+            )
+    )
+
+    private static Item.Settings add(Item.Settings settings) {
+        return settings.fireproof();
+    }
+}
