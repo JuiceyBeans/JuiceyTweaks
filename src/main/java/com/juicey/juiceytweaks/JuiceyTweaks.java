e0f9825f6d07c89eb408ddadfcb24577186b625c
Alyx <75553966+JuiceyBeans@users.noreply.github.com>
2024-02-01 17:09:47 +0000

    Added disclaimer

--- src/main/java/com/juicey/juiceytweaks/JuiceyTweaks.java
+++ src/main/java/com/juicey/juiceytweaks/JuiceyTweaks.java
@@ -17,14 +17,18 @@ import org.slf4j.LoggerFactory;
 
 public class JuiceyTweaks implements ModInitializer {
 
+	// Hello!
+	// If you're reading my code for some reason, please keep in mind that I'm a psych student who makes Minecraft mods as a hobby lol
+	// These mods are mainly made for me and my friends first, public distribution comes second
+	// So a lot of the code here is probably WILDLY unoptimized or just plain stupid
+	// Juicey's Tweaks is my first mod so a lot of things are going to be wonky!
+	// If I ever have the time or motivation to come back to it when I have more experience, I'll try to fix a lot of things that are wrong with it!
+	// Lastly, CONSTRUCTIVE criticism would be appreciated instead of simply calling me dumb for how my code is written :)
+	// Cheers, KarthiBeans
+
     public static final Logger LOGGER = LoggerFactory.getLogger("juiceytweaks");
 	public static final String MOD_ID = "juiceytweaks";
 
-	public static boolean powderSnowWalkCondition(Entity entity) {
-        if(EnchantmentHelper.getEquipmentLevel(Enchantments.FROST_WALKER, ((LivingEntity)entity)) > 0) {
-			return true;
-		} else return false;
-    };
 
 	@Override
 	public void onInitialize() {
